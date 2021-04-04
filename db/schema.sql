64a9b08e88db338bc6fb2d4bdcae451efec60d9a
Bryan Goggin <bgoggin88@gmail.com>
2021-04-04 13:38:07 -0500

    dp set up fixes #2 #3 #4

---
+++ db/schema.sql
@@ -0,0 +1,50 @@
+-- DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    passcode TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE IF NOT EXISTS recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    recipe_name TEXT NOT NULL,
+    instructions TEXT NOT NULL
+);
+
+-- DROP TABLE IF EXISTS ingredients;
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INTEGER,
+    recipe_name TEXT NOT NULL,
+    ingredient TEXT NOT NULL,
+    amount TEXT NOT NULL,
+    PRIMARY KEY (id, recipe_name)
+);
+
+
+-- DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER,
+    recipe_name TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by TEXT NOT NULL,
+    review_score INTEGER NOT NULL,
+    review_text TEXT NOT NULL,
+    PRIMARY KEY ( id, recipe_name)
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+);
